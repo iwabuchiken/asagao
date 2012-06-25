78e36e77b5b6a9668937549518c5ff069aa36d90
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-06-25 12:40:27 +0900

    B=master
    v=1
    d=1
    
    [d]------------------------------------
    1. Up to: "こんにちは"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asagao::Application.config.secret_token = '0ba90ed0dc7896e0a1c8a7cd3b42d545a4f7ecc6d0d4ea42ddb9a96e635f45fa943548227d4fe52954c3157506bc20a0278b67340b8af0706940856ffed4583d'
